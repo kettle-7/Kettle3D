8fa0cff644188aeb4d46fd5d1607e2dbcede6f6c
Kettle3D <64388266+Kettle3D@users.noreply.github.com>
2020-04-27 18:11:11 +1200

    Updated the updater.

--- kettle3D-updater.py
+++ kettle3D-updater.py
@@ -1,13 +1,29 @@
 from urllib.request import urlopen
-
-launcherprogramming = urlopen("https://Kettle3D.github.io/Kettle3D/kettle3D-versions.py").read().decode('utf-8')
+import sys
 
 try:
-  launcher = open("C:\\Program Files\\Kettle3D\\kettle3D-launcher.py", 'w')
-except:
-  launcher = open("C:\\Program Files\\Kettle3D\\kettle3D-launcher.py", 'x')
+	launcherprogramming = urlopen("https://Kettle3D.github.io/Kettle3D/kettle3D-versions.py").read().decode('utf-8')
+	print("Checking for updates")
+	
+	try:
+		launcher = open("C:\\Program Files\\Kettle3D\\kettle3DLauncher.py", '+')
+		old_launcher = launcher.read()
+		is_new = False
+	except:
+		launcher = open("C:\\Program Files\\Kettle3D\\kettle3DLauncher.py", 'x')
+		is_new = True
 
-launcher.write(launcherprogramming)
-launcher.close()
+	launcher.write(launcherprogramming)
+	launcher.close()
+	if is_new:
+		print("Successfully installed Kettle3D launcher")
+	elif old_launcher != launcherprogramming:
+		print("Updated Kettle3D Successfully")
+	else:
+		print("Kettle3D is up to date")
+#except:
+#	print("Kettle3D couldn't check for updates. Try checking your internet connection.")
 
-import kettle3D-launcher
+sys.path.append("C:\\Program Files\\Kettle3D")
+
+import kettle3DLauncher
