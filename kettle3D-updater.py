d196a7cb0156fe1e3583252bed1eb57ebf455cf8
Kettle3D <64388266+Kettle3D@users.noreply.github.com>
2020-04-27 18:46:22 +1200

    Fixed a bug causing crashes when there's no internet.

--- kettle3D-updater.py
+++ kettle3D-updater.py
@@ -2,27 +2,28 @@ from urllib.request import urlopen
 import sys
 
 try:
-	launcherprogramming = urlopen("https://Kettle3D.github.io/Kettle3D/kettle3D-versions.py").read().decode('utf-8')
-	print("Checking for updates")
-	
-	try:
-		launcher = open("C:\\Program Files\\Kettle3D\\kettle3DLauncher.py", '+')
-		old_launcher = launcher.read()
-		is_new = False
-	except:
-		launcher = open("C:\\Program Files\\Kettle3D\\kettle3DLauncher.py", 'x')
-		is_new = True
+        launcherprogramming = urlopen("https://Kettle3D.github.io/Kettle3D/kettle3D-versions.py").read().decode('utf-8')
+        print("Checking for updates")
 
-	launcher.write(launcherprogramming)
-	launcher.close()
-	if is_new:
-		print("Successfully installed Kettle3D launcher")
-	elif old_launcher != launcherprogramming:
-		print("Updated Kettle3D Successfully")
-	else:
-		print("Kettle3D is up to date")
-#except:
-#	print("Kettle3D couldn't check for updates. Try checking your internet connection.")
+        try:
+        	launcher = open("C:\\Program Files\\Kettle3D\\kettle3DLauncher.py", 'w')
+        	o = open("C:\\Program Files\\Kettle3D\\kettle3DLauncher.py", 'r')
+        	old_launcher = o.read()
+        	is_new = False
+        except:
+        	launcher = open("C:\\Program Files\\Kettle3D\\kettle3DLauncher.py", 'x')
+        	is_new = True
+
+        launcher.write(launcherprogramming)
+        launcher.close()
+        if is_new:
+        	print("Successfully installed Kettle3D launcher")
+        elif not old_launcher == launcherprogramming:
+        	print("Updated Kettle3D Successfully")
+        else:
+        	print("Kettle3D is up to date")
+except:
+	print("Kettle3D couldn't check for updates. Try checking your internet connection.")
 
 sys.path.append("C:\\Program Files\\Kettle3D")
 
