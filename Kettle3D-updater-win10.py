5a092b6bc26dd379e9d4c6d1e174455e944020c6
Kettle3D <64388266+Kettle3D@users.noreply.github.com>
2020-04-29 12:56:54 +1200

    Bug Fixes

--- Kettle3D-updater-win10.py
+++ Kettle3D-updater-win10.py
@@ -11,37 +11,37 @@ o = None
 
 try:
 #if True:
-        launcherprogramming = urlopen("https://raw.githubusercontent.com/Kettle3D/Kettle3D/master/kettle3D-versions.py").read().decode('utf-8')
-        print("Checking for updates")
+	launcherprogramming = urlopen("https://raw.githubusercontent.com/Kettle3D/Kettle3D/master/kettle3D-versions.py").read().decode('utf-8')
+	print("Checking for updates")
 
-        try:
-	        if osname == 'windows':
-        		launcher = open("C:\\Program Files\\Kettle3D\\kettle3DLauncher.py", 'w')
-        		o = open("C:\\Program Files\\Kettle3D\\kettle3DLauncher.py", 'r')
-		        os.path[0] = getenv("appdata") + "\\Kettle3D"
-	        elif osname == 'os x':
-                        launcher = open(getenv("HOME") + "/Library/Application Support/Kettle3D/kettle3DLauncher.py", "w")
-                        o = open(getenv("HOME") + "/Library/Application Support/Kettle3D/kettle3DLauncher.py", 'r')
-		        os.path[0] = getenv("HOME") + "/Library/Application Support/Kettle3D"
-        	old_launcher = o.read()
-        	is_new = False
-        except FileNotFoundError:
-                if osname == 'windows':
-                        launcher = open("C:\\Program Files\\Kettle3D\\kettle3DLauncher.py", 'x')
-		        os.path[0] = getenv("appdata") + "\\Kettle3D"
-	        if osname == 'os x':
-		        launcher = open(getenv("HOME") + "/Library/Application Support/Kettle3D/Kettle3D/kettle3DLauncher.py", 'x')
-		        os.path[0] = getenv("HOME") + "/Library/Application Support/Kettle3D"
-        	is_new = True
-
-        launcher.write(launcherprogramming)
-        launcher.close()
-        if is_new:
-        	print("Successfully installed Kettle3D launcher")
-        elif not old_launcher == launcherprogramming:
-        	print("Updated Kettle3D Successfully")
-        else:
-        	print("Kettle3D is up to date")
+	try:
+		if osname == 'windows':
+			launcher = open("C:\\Program Files\\Kettle3D\\kettle3DLauncher.py", 'w')
+			o = open("C:\\Program Files\\Kettle3D\\kettle3DLauncher.py", 'r')
+			os.path[0] = getenv("appdata") + "\\Kettle3D"
+		elif osname == 'os x':
+			launcher = open(getenv("HOME") + "/Library/Application Support/Kettle3D/kettle3DLauncher.py", "w")
+			o = open(getenv("HOME") + "/Library/Application Support/Kettle3D/kettle3DLauncher.py", 'r')
+			os.path[0] = getenv("HOME") + "/Library/Application Support/Kettle3D"
+		old_launcher = o.read()
+		is_new = False
+	except FileNotFoundError:
+		if osname == 'windows':
+			launcher = open("C:\\Program Files\\Kettle3D\\kettle3DLauncher.py", 'x')
+			os.path[0] = getenv("appdata") + "\\Kettle3D"
+		if osname == 'os x':
+			launcher = open(getenv("HOME") + "/Library/Application Support/Kettle3D/Kettle3D/kettle3DLauncher.py", 'x')
+			os.path[0] = getenv("HOME") + "/Library/Application Support/Kettle3D"
+		is_new = True
+	
+	launcher.write(launcherprogramming)
+	launcher.close()
+	if is_new:
+		print("Successfully installed Kettle3D launcher")
+	elif not old_launcher == launcherprogramming:
+		print("Updated Kettle3D Successfully")
+	else:
+		print("Kettle3D is up to date")
 except:
 	print("Kettle3D couldn't check for updates. Try checking your internet connection.")
 
