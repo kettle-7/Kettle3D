794168bc1ea989f06240d490320a606031f17373
Kettle3D <64388266+Kettle3D@users.noreply.github.com>
2020-04-29 12:30:16 +1200

    Bug Fixes

--- Kettle3D-updater-win10.py
+++ Kettle3D-updater-win10.py
@@ -19,7 +19,7 @@ try:
         		launcher = open("C:\\Program Files\\Kettle3D\\kettle3DLauncher.py", 'w')
         		o = open("C:\\Program Files\\Kettle3D\\kettle3DLauncher.py", 'r')
 		        os.path[0] = getenv("appdata") + "\\Kettle3D"
-		elif osname == 'os x':
+	        elif osname == 'os x':
 			launcher = open(getenv("HOME") + "/Library/Application Support/Kettle3D/kettle3DLauncher.py", "w")
 			o = open(getenv("HOME") + "/Library/Application Support/Kettle3D/kettle3DLauncher.py", 'r')
 			os.path[0] = getenv("HOME") + "/Library/Application Support/Kettle3D"
