56b44ed01a0e75b3540019e7e15e1a4155894bfe
Kettle3D <64388266+Kettle3D@users.noreply.github.com>
2020-05-01 13:52:33 +1200

    Update d2005a.py

--- versions/d2005a.py
+++ versions/d2005a.py
@@ -95,9 +95,10 @@ except ModuleNotFoundError:
 
 # All versions need the above code.
 
-def launch_k3d(self=None):
+def launch_k3d(self=None): # worlds etc. need to be passed in as parameters here.
   	# The code below executes when you open the version with the launcher.
   
-  	print("Kettle3D development version d20-04 build A launched.")
+  	print("Kettle3D development version d20-05 build A launched.")
 	
 	#This version currently doesn't do anything - it's like that for a reason.
+	
