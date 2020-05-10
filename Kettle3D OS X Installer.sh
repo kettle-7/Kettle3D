a135e91d78b11fd09e8bcf342784e34c82bfe1f3
Kettle3D <64388266+Kettle3D@users.noreply.github.com>
2020-05-10 16:35:34 +1200

    Changed Mac scripting files to have Mac EOL

--- Kettle3D OS X Installer.sh
+++ Kettle3D OS X Installer.sh
@@ -1,22 +1,1 @@
-# Install Panda
-
-cd $HOME/Downloads
-curl -o panda_installer.dmg https://www.panda3d.org/download/panda3d-1.10.6/Panda3D-SDK-1.10.6-MacOSX10.6.dmg
-hdiutil attach panda_installer.dmg
-
-# Install Kettle3D
-cd $HOME
-mkdir Library
-cd Library
-mkdir "Application Support"
-cd "Application Support"
-mkdir Kettle3D
-cd Kettle3D
-mkdir assets
-mkdir data
-mkdir lib
-mkdir versions
-curl -o kettle3D-updater.py https://raw.githubusercontent.com/Kettle3D/Kettle3D/master/Kettle3D-updater-win10.py
-curl -o Kettle3D.sh https://raw.githubusercontent.com/Kettle3D/Kettle3D/master/Kettle3D.sh
-cd $HOME/Desktop
-curl -o Kettle3D.command https://raw.githubusercontent.com/Kettle3D/Kettle3D/master/Kettle3D.sh
+# Install Pandacd $HOME/Downloadscurl -o panda_installer.dmg https://www.panda3d.org/download/panda3d-1.10.6/Panda3D-SDK-1.10.6-MacOSX10.6.dmghdiutil attach panda_installer.dmg# Install Kettle3Dcd $HOMEmkdir Librarycd Librarymkdir "Application Support"cd "Application Support"mkdir Kettle3Dcd Kettle3Dmkdir assetsmkdir datamkdir libmkdir versionscurl -o kettle3D-updater.py https://raw.githubusercontent.com/Kettle3D/Kettle3D/master/Kettle3D-updater-win10.pycurl -o Kettle3D.sh https://raw.githubusercontent.com/Kettle3D/Kettle3D/master/Kettle3D.shcd $HOME/Desktopcurl -o Kettle3D.command https://raw.githubusercontent.com/Kettle3D/Kettle3D/master/Kettle3D.sh
--- Kettle3D.sh
+++ Kettle3D.sh
@@ -1,2 +1,1 @@
-cd "$HOME/Library/Application Support/Kettle3D"
-python3 kettle3D-updater.py
+cd "$HOME/Library/Application Support/Kettle3D"python3 kettle3D-updater.py
