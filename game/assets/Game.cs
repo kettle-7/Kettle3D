1c3b8499bf1b656e6fd6b5fc32ddfd0767548072
Kettle3D <64388266+Kettle3D@users.noreply.github.com>
2020-06-09 09:41:03 +1200

    Fixed components.json

--- game/assets/Game.cs
+++ game/assets/Game.cs
@@ -8,18 +8,20 @@ public class Game : MonoBehaviour
 {
     public GameObject ConcreteModel;
     public GameObject GrassModel;
+    public GameObject DirtModel;
+    public GameObject StoneModel;
+    public GameObject K3DModel;
     public float xpos, ypos, zpos, movex, movey, movez;
-    public TextMesh xXxXxX, yYyYyY, zZzZzZ;
 
     void Start()
     {
         var array = new int[16];
         List<GameObject> worldmap = new List<GameObject>();
-        // Instantiate with the given coordinates and no rotation.
         foreach (var item in array)
         {
             foreach (var item2 in array)
             {
+                // Instantiate with the given coordinates and no rotation.
                 worldmap.Add(Instantiate(ConcreteModel, new Vector3(item, -1.5f, item2), Quaternion.identity));
             }
         }
@@ -35,47 +37,10 @@ public class Game : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        GameObject.Find("Main Camera").transform.rotation = Quaternion.Euler(Input.mousePosition.y,Input.mousePosition.x,0);
-
-        if (Input.GetKeyDown(KeyCode.W)) {
-            this.movex = this.movex / 0.73f;
-            this.movez = this.movez / 0.73f;
-            this.movez += 0.25f;
-            this.xpos += this.movex;
-            this.zpos += this.movez;
-            this.move();
-        } else if (Input.GetKeyDown(KeyCode.A)) {
-            this.movex = this.movex / 0.73f;
-            this.movez = this.movez / 0.73f;
-            this.movex -= 0.25f;
-            this.xpos += this.movex;
-            this.zpos += this.movez;
-            this.move();
-        } else if (Input.GetKeyDown(KeyCode.S)) {
-            this.movex = this.movex / 0.73f;
-            this.movez = this.movez / 0.73f;
-            this.movez -= 0.25f;
-            this.xpos += this.movex;
-            this.zpos += this.movez;
-            this.move();
-        } else if (Input.GetKeyDown(KeyCode.D)) {
-            this.movex = this.movex / 0.73f;
-            this.movez = this.movez / 0.73f;
-            this.movex += 0.25f;
-            this.xpos += this.movex;
-            this.zpos += this.movez;
-            this.move();
-        } else {
-            this.movex = this.movex / 0.73f;
-            this.movez = this.movez / 0.73f;
-            this.xpos += this.movex;
-            this.zpos += this.movez;
-            this.move();
-        }
-
-        this.xXxXxX.text = "X: pos: " + this.xpos.ToString() + "velocity: " + this.movex.ToString();
-        this.yYyYyY.text = "Y: pos: " + this.ypos.ToString() + "velocity: " + this.movey.ToString();
-        this.zZzZzZ.text = "Z: pos: " + this.zpos.ToString() + "velocity: " + this.movez.ToString();
+        this.movex = Input.GetAxis("Horizontal");
+        this.movez = Input.GetAxis("Vertical");
+        GameObject.Find("Main Camera").transform.Rotate(Input.mousePosition.y,Input.mousePosition.x,0);
+        GameObject.Find("Main Camera").transform.Translate(this.movex, this.movey, this.movez);
     }
 
     void move()
