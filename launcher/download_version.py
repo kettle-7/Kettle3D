49497f90f583619a60a0bfc877cd4dd8e0c4754b
Kettle3D <Kettle3D@noreply.github.com>
2020-09-19 13:02:16 +1200

    Started work on the new Python launcher.

---
+++ launcher/download_version.py
@@ -0,0 +1,12 @@
+# Interprets the version information
+
+class interpreter: # Basic class for interpreting downloaded files
+    def __init__ (self):
+        pass
+
+    def parseline(self, line):
+        pass
+
+    @staticmethod
+    def getversion(version, mod_diff, game_diff):
+        i = interpreter()
